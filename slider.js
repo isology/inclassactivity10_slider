898227801f0c9e2fc5c00626addfa46c89874f1d
isology <isoalper-068@hotmail.com>
2024-05-10 15:32:01 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 2,
+			  maxSlides: 2,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  maxSlides: 1,
+			  pause: 3000,
+			  pager: true
+			});
+			
+			
+    	});
